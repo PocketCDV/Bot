9aa63addaa064d5549d85ee2b8087693f92caae5
Plummy <plummybeatsoff@gmail.com>
2026-04-24 18:02:10 +0200

    Minor fixes

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -9,45 +9,11 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram_i18n.cores import FluentCompileCore
 from redis.asyncio import Redis
-from sqlalchemy.dialects.postgresql import insert
 
 from app.bot.actions.home import HomeAction
 from app.bot.middlewares.message_id import UserMessage
 from app.bot.utils import get_state
 from app.celery.worker import worker, config
-from app.database.database import Database
-from app.database.models import User
-
-
-async def __async_insert_user_to_database(
-        telegram_id: int,
-        first_name: str,
-        locale: str,
-) -> None:
-    database: Database = Database.from_dsn(config.database_dsn.get_secret_value())
-
-    async with database.session_maker() as database_session:
-        await database_session.execute(
-            insert(User).values(
-                telegram_id=telegram_id,
-                first_name=first_name,
-                locale=locale,
-            ).on_conflict_do_nothing(index_elements=["telegram_id"])
-        )
-        await database_session.commit()
-
-
-async def __async_update_state(
-        telegram_id: int,
-        data: Dict[str, Any],
-) -> None:
-    state: FSMContext = get_state(
-        Redis.from_url(config.redis_dsn.get_secret_value()),
-        Bot(token=config.telegram_bot_token.get_secret_value()),
-        telegram_id,
-    )
-
-    await state.update_data(data)
 
 
 async def __async_set_successful_login_message(

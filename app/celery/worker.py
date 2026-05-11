23d5f0633d0e2509351d57144e89ed3037689100
Plummy <plummybeatsoff@gmail.com>
2026-05-11 19:05:30 +0200

    */5 instead of manually entering cron timings

--- app/celery/worker.py
+++ app/celery/worker.py
@@ -40,7 +40,7 @@ def create_worker() -> Celery:
         },
         "cron_dispatch_notifications": {
             "task": "dispatch_notifications",
-            "schedule": crontab(minute="2,7,12,17,22,27,32,37,42,47,52,57"),
+            "schedule": crontab(minute="*/5"),
         },
     }
 
